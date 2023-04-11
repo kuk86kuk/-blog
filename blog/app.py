c4d9a949234ff9746cb0866e35cbb6020bdc5626
Treglaov <egort,123@yandex.ru>
2023-04-11 19:05:21 +0500

    Home_work2

--- blog/app.py
+++ blog/app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello web!"
 
+@app.route("/")
+@app.route("/home")
+def home(name=None):
+    return render_template('index.html', name=name)
+
+
+@app.route("/blog")
+def blog(name=None):
+    return render_template('blog.html', name=name)
+
+
+@app.route("/cars")
+def cars(name=None):
+    return render_template('cars.html', name=name)
+
+
+@app.route("/messages")
+def messages(name=None):
+    return render_template('messages.html', name=name)
+
+
+@app.route("/abaut")
+def abaut(name=None):
+    return render_template('abaut.html', name=name)
+
+
+@app.route("/login")
+def login(name=None):
+    return render_template('login.html', name=name)
 
 @app.route("/greet/<name>/")
 def greet_name(name: str):
--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,5 @@ from blog.app import app
 
 if __name__ == "__main__":
     app.run(
-        host="0.0.0.0",
         debug=True,
         )
