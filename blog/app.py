d9e66e44d7851faec24195631efb239ac70b94a7
Treglaov <egort,123@yandex.ru>
2023-04-11 21:49:30 +0500

    Home_work3

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+
 
 
 app = Flask(__name__)
@@ -37,3 +44,9 @@ def login(name=None):
 @app.route("/greet/<name>/")
 def greet_name(name: str):
     return f"Hello {name}!"
+
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,12 @@
-from blog.app import app
+from blog.app import app, db
 
 if __name__ == "__main__":
     app.run(
         debug=True,
         )
+
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+    print("done!")
